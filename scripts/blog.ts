a0914391c75979d8d9f6063c35cced0f77ee1460
Xavier Garcia <xmangarcia17@gmail.com>
2023-10-18 16:53:58 -0700

    feat: add more files

---
+++ scripts/blog.ts
@@ -0,0 +1,21 @@
+type Blog = {
+  title: string;
+  date: string;
+  description: string;
+  slug: string;
+};
+
+const blogs: Blog[] = [
+  {
+    title: "Blog Intro",
+    date: "10-18-2023",
+    description: "This is my intro to my blog on this website.",
+    slug: "",
+  },
+  {
+    title: "First Blog",
+    date: "10-18-2023",
+    description: "This is my first blog on this website.",
+    slug: "",
+  },
+];
