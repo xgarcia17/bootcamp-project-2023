0de75630d4befaae348e16a935bc3aba02da6eb0
Xavier Garcia <xmangarcia17@gmail.com>
2023-12-22 12:34:05 -0800

    feat: change directories in navbar

--- Milestone3B/src/app/components/navbar.tsx
+++ Milestone3B/src/app/components/navbar.tsx
@@ -8,13 +8,13 @@ export default function Navbar() {
     // with your navbar code from your earlier milestones
     <header className={style.navbar} >
       <nav>
-        <h1 className={style.logo}><Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/">Xavier&apos;s Personal Website</Link></h1>
+        <h1 className={style.logo}><Link href="/">Xavier&apos;s Personal Website</Link></h1>
             <ul className={style.navlist}>
-                <Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/">Home</Link>
-                <Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/blog">Blogs</Link>
-                <Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/portfolio">Portfolio</Link>
-                <Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/resume">Resume</Link>
-                <Link href="https://xaviers-personal-website-gf35kvjry-xgarcia17s-projects.vercel.app/contact">Contact</Link>
+                <Link href="/">Home</Link>
+                <Link href="/blog">Blogs</Link>
+                <Link href="/portfolio">Portfolio</Link>
+                <Link href="/resume">Resume</Link>
+                <Link href="/contact">Contact</Link>
                 {/* <Link href="http://localhost:3000/">Home</Link> */}
                 {/* <Link href="http://localhost:3000/blog">Blogs</Link> */}
                 {/* <Link href="http://localhost:3000/portfolio">Portfolio</Link> */}
